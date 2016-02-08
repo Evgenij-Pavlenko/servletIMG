d6cd24d6f463a79d24d63a4b02903295cc1613c6
Evgenij-Pavlenko <evgenij.pavlenko@gmail.com>
2016-02-08 04:58:12 +0200

    только форма загрузки

---
+++ src/ua/org/oa/pavlenko_e/picture.java
@@ -0,0 +1,21 @@
+package ua.org.oa.pavlenko_e;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Servlet implementation class picture
+ */
+@WebServlet("/picture")
+public class picture extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public picture() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
